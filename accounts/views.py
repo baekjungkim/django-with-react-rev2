4d336656f553d6b2a240ed2f84bc145364541109
baekjung <zvgandam@gmail.com>
2020-07-17 15:13:50 +0900

    07 로그인-로그아웃 구현 그리고 회원 가입과 동시에 로그인05 회원 가입 환영 이메일 보내기

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,27 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+# 로그아웃 하자마자 로그인으로 보냄
+def logout(request):
+    messages.success(request, "로그아웃 되었습니다.")
+    return logout_then_login(request)
 
 
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
-            singed_user = form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입 환영합니다.")
-            singed_user.send_welcome_email()  # FIXME: Celery로 처리하는것을 추천
+            signed_user.send_welcome_email()  # FIXME: Celery로 처리하는것을 추천
             next_url = request.GET.get("next", "/")
             return redirect(next_url)
     else:
