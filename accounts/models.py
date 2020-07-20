c362f09100af4c20c3f570ff603e334083094a6c
baekjung <zvgandam@gmail.com>
2020-07-20 13:33:33 +0900

    09 프로필 수정 구현과 장고3에서 추가된 TextChoices 활용

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,29 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
+from django.core.validators import RegexValidator
 from django.db import models
 from django.template.loader import render_to_string
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(
+        max_length=13,
+        validators=[RegexValidator(r"^01(?:0|1|[6-9])[.-]?(\d{3}|\d{4})[.-]?(\d{4})$")],
+        blank=True,
+    )
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices, blank=True)
+    avatar = models.ImageField(
+        upload_to="accounts/avatar/%Y/%m/%d",
+        help_text="48px * 48px 크기의 png/jpg 파일을 업로드 해주세요.",
+        blank=True,
+    )
 
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {"user": self})
