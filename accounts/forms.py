e357538e9c2dc3f788f42a9d05642d7ced1e9dfb
baekjung <zvgandam@gmail.com>
2020-07-20 14:49:23 +0900

    10 auth앱의 PasswordChangeForm 커스텀을 통한 암호 수정 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    PasswordChangeForm as AuthPasswordChangeForm,
+)
 from .models import User
 
 
@@ -41,4 +44,22 @@ class ProfileForm(forms.ModelForm):
             "phone_number",
             "gender",
         ]
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    # New password 에서 메시지 출력
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get("old_password")
+        new_password1 = self.cleaned_data.get("new_password1")
+        if old_password == new_password1:
+            raise forms.ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+        return new_password1
+
+    # New password confirmation 에서 메시지 출력
+    # def clean_new_password2(self):
+    #     old_password = self.cleaned_data.get("old_password")
+    #     new_password2 = super().clean_new_password2()
+    #     if old_password == new_password2:
+    #         raise forms.ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+    #     return new_password2
 
